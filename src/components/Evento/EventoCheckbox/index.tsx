3e3ffc21ab321eacf3da240fb09ef3b641b6acf1
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-02-27 13:31:45 -0300

    att value date in drag and drop

--- src/components/Evento/EventoCheckbox/index.tsx
+++ src/components/Evento/EventoCheckbox/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { IEvento } from '../../../interfaces/IEvento';
+import { useSetRecoilState } from 'recoil';
+import { listaDeEventosState } from '../../../state/atom';
 
-const EventoCheckbox: React.FC<{ evento: IEvento, aoAlterarStatus: (id: number) => void }> = ({ evento, aoAlterarStatus }) => {
+const EventoCheckbox: React.FC<{ evento: IEvento }> = ({ evento }) => {
   
   const estilos = [
     'far',
@@ -9,7 +11,21 @@ const EventoCheckbox: React.FC<{ evento: IEvento, aoAlterarStatus: (id: number) 
     evento.completo ? 'fa-check-square' : 'fa-square'
   ]
 
-  return (<i className={estilos.join(' ')} onClick={() => aoAlterarStatus(evento.id!)}></i>)
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState)
+
+  const alterarStatus = () => {
+    const eventoAlterado = {
+      ...evento
+    }
+    eventoAlterado.completo = !evento.completo
+
+    setListaDeEventos(listaAntiga => {
+      const indice = listaAntiga.findIndex(e => e.id === evento.id)
+      return [...listaAntiga.slice(0, indice), eventoAlterado, ...listaAntiga.slice(indice + 1)]
+    })
+  }
+
+  return (<i className={estilos.join(' ')} onClick={alterarStatus}></i>)
 }
 
 export default EventoCheckbox
